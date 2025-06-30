3b5f1d2dc5282702cc6554b3a091e0aa51392e81
kethirabalan <kethirabalantvl@gmail.com>
2025-06-30 17:20:35 +0530

    Init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'triplan',
+  appName: 'triplan',
+  webDir: 'www'
+};
+
+export default config;
