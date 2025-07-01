8fceaf49668667bcaa7e98e08a699bd6aee537fc
kethirabalan <kethirabalantvl@gmail.com>
2025-07-01 10:09:03 +0530

    Init

--- src/app/pages/search/search.ts
+++ src/app/pages/search/search.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-search',
   templateUrl: 'search.html',
   styleUrls: ['search.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
 })
 export class Search {
 
--- src/app/pages/trips/trips.ts
+++ src/app/pages/trips/trips.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-trips',
   templateUrl: 'trips.html',
   styleUrls: ['trips.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class Trips {
   constructor() {}
