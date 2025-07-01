b47ddf7da2eba14f2623c7a9f5c40b0bb9a71401
kethirabalan <kethirabalantvl@gmail.com>
2025-07-01 20:28:47 +0530

    init

--- src/app/pages/search/search.ts
+++ src/app/pages/search/search.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonIcon, IonList, IonItem,IonButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonIcon, IonItem,IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-search',
   templateUrl: 'search.html',
   styleUrls: ['search.scss'],
-  imports: [IonHeader, IonContent, IonSearchbar, IonIcon, IonList, IonItem, IonButton]
+  imports: [IonHeader, IonContent, IonSearchbar, IonIcon, IonItem, IonButton]
 })
 export class Search {
 
