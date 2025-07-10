f2b477458e6fccfd70fa46356e2a169065cc22f8
kethirabalan <kethirabalantvl@gmail.com>
2025-07-10 12:54:52 +0530

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'splash', pathMatch: 'full' },
-  { path: 'splash', loadComponent: () => import('./splash/splash.page').then(m => m.SplashPage ) },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', loadComponent: () => import('./splash/splash.page').then(m => m.SplashPage ) },
   {
     path: 'main',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
