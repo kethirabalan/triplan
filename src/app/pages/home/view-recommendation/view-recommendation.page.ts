9935b8e8b9c21cc1d73f4b857857d9a07e3fb901
kethirabalan <kethirabalantvl@gmail.com>
2025-07-12 10:21:47 +0530

    Init

--- src/app/pages/home/view-recommendation/view-recommendation.page.ts
+++ src/app/pages/home/view-recommendation/view-recommendation.page.ts
@@ -59,7 +59,6 @@ export class ViewRecommendationPage implements OnInit {
   }
 
   shareRecommendation() {
-    console.log(window.location.href);
     const shareData = {
       title: this.spot.name,
       text: this.spot.shortDesc,
@@ -94,7 +93,6 @@ export class ViewRecommendationPage implements OnInit {
   }
 
   openMap() {
-    console.log(this.spot);
     const url = this.spot.mapUrl || `https://www.google.com/maps/search/?api=1&query=${this.spot.latitude},${this.spot.longitude}`;
     window.open(url, '_blank');
   }
